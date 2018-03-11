8b3893ea50c00be39b27ed126e5d3b37d6dfc1e1
Mads Anthony (Desktop) <mail@madsanthony.com>
2018-03-11 18:30:42 +0100

    Added an extra animation + hero script.

---
+++ Minion/Assets/Code/HeroScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HeroScript : MonoBehaviour {
+
+	public Animator Animator;
+	// Use this for initialization
+	void Start () {
+		Animator.SetInteger ("stateIndex", (int)(HeroAnimationState.idle));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) {
+			Scene scene = SceneManager.GetActiveScene ();
+			SceneManager.LoadScene (scene.name);
+		}
+
+		Animator.SetInteger ("stateIndex", (int)(HeroAnimationState.idle));
+		if (Input.GetMouseButton(0)) {
+			transform.position += new Vector3 (1,0,1)*Time.deltaTime*-3;
+			Animator.SetInteger ("stateIndex", (int)(HeroAnimationState.run));
+		}
+	}
+
+	enum HeroAnimationState {
+		idle,
+		run
+	}
+}
