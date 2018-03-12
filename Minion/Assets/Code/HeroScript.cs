a293018c9609e5c5f7245ea3e6cf41b675991cea
Mads Anthony (Desktop) <mail@madsanthony.com>
2018-03-12 19:25:54 +0100

    Added the ability to move the hero anywhere on the platform.

--- Minion/Assets/Code/HeroScript.cs
+++ Minion/Assets/Code/HeroScript.cs
@@ -6,11 +6,16 @@ using UnityEngine.SceneManagement;
 public class HeroScript : MonoBehaviour {
 
 	public Animator Animator;
+	public Camera camera;
+
+	private Vector3 targetPos;
 	// Use this for initialization
 	void Start () {
 		Animator.SetInteger ("stateIndex", (int)(HeroAnimationState.idle));
+
+		targetPos = transform.position;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.R)) {
@@ -18,11 +23,41 @@ public class HeroScript : MonoBehaviour {
 			SceneManager.LoadScene (scene.name);
 		}
 
-		Animator.SetInteger ("stateIndex", (int)(HeroAnimationState.idle));
-		if (Input.GetMouseButton(0)) {
-			transform.position += new Vector3 (1,0,1)*Time.deltaTime*-3;
-			Animator.SetInteger ("stateIndex", (int)(HeroAnimationState.run));
+		//Animator.SetInteger ("stateIndex", (int)(HeroAnimationState.idle));
+		if (Input.GetMouseButtonDown(0)) {
+			//transform.position += new Vector3 (1,0,1)*Time.deltaTime*-3;
+
+
+			RaycastHit hit;
+			Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(ray, out hit)) {
+				Transform objectHit = hit.transform;
+
+				targetPos = new Vector3 (hit.point.x, transform.position.y, hit.point.z);
+				StartCoroutine (Move ());
+				// Do something with the object that was hit by the raycast.
+			}
 		}
+		//transform.position += (targetPos-transform.position)*Time.deltaTime;
+	}
+
+	IEnumerator Move() {
+		float t = 0;
+		var startPos = transform.position;
+		Animator.SetInteger ("stateIndex", (int)(HeroAnimationState.run));
+
+		var dirVector = targetPos-startPos;
+		transform.localEulerAngles = new Vector3 (0,Mathf.Atan2(dirVector.x,dirVector.z)*Mathf.Rad2Deg-45+180,0);
+		while (true) {
+			t += Time.deltaTime;
+			transform.position = Vector3.Lerp (startPos, targetPos, t);
+			if (t>1) {
+				break;
+			}
+			yield return null;
+		}
+		Animator.SetInteger ("stateIndex", (int)(HeroAnimationState.idle));
 	}
 
 	enum HeroAnimationState {
