6681d3138f3c9af3640a24ed1e881fdb01598cec
Mads Anthony (Desktop) <mail@madsanthony.com>
2018-03-27 22:09:36 +0200

    Started working on leveleditor.

---
+++ Minion/Assets/Code/LevelAsset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor;
+
+[CreateAssetMenu(fileName = "Level", menuName = "Assets/New Level", order = 1)]
+public class LevelAsset : ScriptableObject {
+	public List<Tile> tiles;
+
+	public class Tile {
+	}
+}
---
+++ Minion/Assets/Code/LevelInspector.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelAsset))]
+public class LevelInspector : Editor {
+	private RenderTexture editorRenderTexture;
+	private Vector2 windowOffset = new Vector2(20,100);
+	private Vector2 windowSize = new Vector2(500,500);
+
+	Vector2 startMousePos;
+	Vector2 mousePos;
+	public override void OnInspectorGUI()
+	{
+		LevelAsset myTarget = (LevelAsset)target;
+		if (cameraGameObject != null) {
+			EditorGUI.DrawPreviewTexture (new Rect (0+windowOffset.x, 0+windowOffset.y, windowSize.x, windowSize.y), editorRenderTexture);
+		}
+		//EditorGUI.DrawRect (new Rect (10, 10, 60, 60), Color.blue);
+		//if (Event.current.type == EventType.MouseDrag) {
+
+		if (Event.current.button == 2) {
+			//mousePos = Event.current.mousePosition;
+			if (Event.current.type == EventType.MouseDown) {
+				mousePos = Event.current.mousePosition;
+			}
+			if (Event.current.type == EventType.MouseDrag) {
+				var mouseDir = (mousePos - Event.current.mousePosition) * 0.01f;
+				cameraGameObject.transform.position += new Vector3 (mouseDir.x, -mouseDir.y, 0);
+				mousePos = Event.current.mousePosition;
+			}
+			//cameraGameObject.transform.position += new Vector3 (0.2f, 0, 0);
+		}
+		//}
+		EditorUtility.SetDirty (myTarget);
+	}
+
+	private void OnEnable() {
+		ConstructLevel ();
+	}
+
+	private void OnDisable() {
+		DestroyLevel ();
+	}
+
+	private GameObject cameraGameObject;
+	private void ConstructLevel() {
+		cameraGameObject = new GameObject();
+		cameraGameObject.transform.position = new Vector3 (0,0,-12);
+		cameraGameObject.hideFlags = HideFlags.HideAndDontSave;
+		var camera = cameraGameObject.AddComponent<Camera> ();
+		editorRenderTexture = Resources.Load ("EditorRenderTexture") as RenderTexture;
+		camera.targetTexture = editorRenderTexture;
+	}
+
+	private void DestroyLevel() {
+		GameObject.DestroyImmediate (cameraGameObject);
+	}
+}
