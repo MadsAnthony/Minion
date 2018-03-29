0b8944b6171598054e096e4e5d9c369d9c4bd80a
Mads Nørgaard Anthony <mail@madsanthony.com>
2018-03-29 22:12:34 +0200

    In the LevelInspector you can place and remove tiles.

--- Minion/Assets/Code/Editor/LevelInspector.cs
+++ Minion/Assets/Code/Editor/LevelInspector.cs
@@ -8,17 +8,39 @@ public class LevelInspector : Editor {
 	private Vector2 windowOffset = new Vector2(20,100);
 	private Vector2 windowSize = new Vector2(500,500);
 	private Color windowBackgroundColor = new Color (1,1,1,1);
+	private int windowGridSize = 100;
 
 	Vector2 startMousePos;
 	Vector2 mousePos;
 	public override void OnInspectorGUI()
 	{
+		bool reconstruct = false;
 		LevelAsset myTarget = (LevelAsset)target;
 		if (cameraGameObject != null) {
 			EditorGUI.DrawPreviewTexture (new Rect (0+windowOffset.x, 0+windowOffset.y, windowSize.x, windowSize.y), editorRenderTexture);
 		}
-		//EditorGUI.DrawRect (new Rect (10, 10, 60, 60), Color.blue);
-		//if (Event.current.type == EventType.MouseDrag) {
+
+		var tmpMousePos = Event.current.mousePosition;
+		tmpMousePos -= windowOffset;
+		tmpMousePos -= windowSize * 0.5f;
+		var mousePosInGrid = new Vector3(Mathf.RoundToInt(tmpMousePos.x / windowGridSize), 0, -Mathf.RoundToInt(tmpMousePos.y / windowGridSize));
+		if (Event.current.type == EventType.MouseDown) {
+			
+			if (Event.current.button == 0) {
+				if (!myTarget.tiles.Exists(x => { return x.pos == mousePosInGrid; })) {
+					myTarget.tiles.Add(new TileData(mousePosInGrid));
+					reconstruct = true;
+				}
+			}
+			
+			if (Event.current.button == 1){
+				var posibleTile = myTarget.tiles.Find(x => { return x.pos == mousePosInGrid; });
+				if (posibleTile != null) {
+					myTarget.tiles.Remove(posibleTile);
+					reconstruct = true;
+				}
+			}
+		}
 
 		if (Event.current.button == 2) {
 			//mousePos = Event.current.mousePosition;
@@ -27,15 +49,29 @@ public class LevelInspector : Editor {
 			}
 			if (Event.current.type == EventType.MouseDrag) {
 				var mouseDir = (mousePos - Event.current.mousePosition) * 0.01f;
-				cameraGameObject.transform.position += new Vector3 (mouseDir.x, -mouseDir.y, 0);
+				cameraGameObject.transform.position += new Vector3 (mouseDir.x, 0, -mouseDir.y);
 				mousePos = Event.current.mousePosition;
 			}
-			//cameraGameObject.transform.position += new Vector3 (0.2f, 0, 0);
 		}
-		//}
+
+		if (reconstruct) {
+			DestroyLevel ();
+			ConstructLevel();
+		}
 		EditorUtility.SetDirty (myTarget);
 	}
 
+	private bool TilesContainsPos(Vector3 pos) {
+		LevelAsset myTarget = (LevelAsset)target;
+		foreach (var tile in myTarget.tiles) {
+			if (tile.pos == pos) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+	
 	private void OnEnable() {
 		ConstructLevel ();
 	}
@@ -45,19 +81,46 @@ public class LevelInspector : Editor {
 	}
 
 	private GameObject cameraGameObject;
+	private GameObject rootContainer;
 	private void ConstructLevel() {
+		LevelAsset myTarget = (LevelAsset)target;
+		rootContainer = new GameObject();
+		rootContainer.transform.position = new Vector3 (0,0,-100);
+		
 		cameraGameObject = new GameObject();
-		cameraGameObject.transform.position = new Vector3 (0,0,-12);
-		cameraGameObject.hideFlags = HideFlags.HideAndDontSave;
+		cameraGameObject.transform.position = new Vector3 (0,4,-100);
+		cameraGameObject.transform.eulerAngles = new Vector3(90,0,0);
 		var camera = cameraGameObject.AddComponent<Camera> ();
 		camera.orthographic = true;
 		camera.clearFlags = CameraClearFlags.Color;
 		camera.backgroundColor = windowBackgroundColor;
 		editorRenderTexture = Resources.Load ("EditorRenderTexture") as RenderTexture;
 		camera.targetTexture = editorRenderTexture;
+
+		foreach (var tile in myTarget.tiles) {
+			var gameObjectTile = GameObject.Instantiate(Resources.Load<GameObject>("Tile"));
+			AssignObjectToGrid(gameObjectTile,(int)tile.pos.x,(int)tile.pos.z);
+		}
+
+		SetHideFlagsRecursively(rootContainer);
 	}
 
+	public void SetHideFlagsRecursively(GameObject gameObject) {
+		gameObject.hideFlags = HideFlags.HideAndDontSave;
+		foreach (Transform child in gameObject.transform) {
+			SetHideFlagsRecursively(child.gameObject);
+		}
+	}
+	
+	void AssignObjectToGrid(GameObject newObject, int x, int z) {
+		newObject.transform.parent = rootContainer.transform;
+		newObject.transform.localEulerAngles = new Vector3 (0,0,0);
+		newObject.transform.localPosition = new Vector3 (x*2,0,z*2);
+		//grid [x, z].objects.Add (newObject);
+	}
+	
 	private void DestroyLevel() {
+		GameObject.DestroyImmediate (rootContainer);
 		GameObject.DestroyImmediate (cameraGameObject);
 	}
 }
--- Minion/Assets/Code/LevelAsset.cs
+++ Minion/Assets/Code/LevelAsset.cs
@@ -1,11 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections.Generic;
-using UnityEditor;
 
 [CreateAssetMenu(fileName = "Level", menuName = "Assets/New Level", order = 1)]
 public class LevelAsset : ScriptableObject {
-	public List<Tile> tiles;
+	public List<TileData> tiles = new List<TileData>();
 
-	public class Tile {
+	
+}
+
+[Serializable]
+public class TileData {
+	public Vector3 pos;
+	public List<TileObject> tileObjects = new List<TileObject>();
+
+	public TileData(Vector3 pos) {
+		this.pos = pos;
 	}
+}
+
+[Serializable]
+public class TileObject {
+	
 }
